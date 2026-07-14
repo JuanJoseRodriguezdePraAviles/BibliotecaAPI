36c00fce397c7f29478e5eaee456665b95db1359
Juan José Rodíguez de Pra Avilés <jjpra196@gmail.com>
2026-07-14 09:42:43 +0200

    UserSystem controller

---
+++ BibliotecaAPI/Controllers/UserSystemController.cs
@@ -0,0 +1,25 @@
+﻿using BibliotecaAPI.Data;
+using BibliotecaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserSystemController : ControllerBase
+    {
+        private readonly BibliotecaContext _context;
+
+        public UserSystemController(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserSystem>> Get()
+        {
+            return await _context.UsersSystem.ToListAsync();
+        }
+    }
+}
--- BibliotecaAPI/obj/Debug/net10.0/BibliotecaAPI.AssemblyInfo.cs
+++ BibliotecaAPI/obj/Debug/net10.0/BibliotecaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9293dd54b67915687860a5d4d201fb937d9911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d628228123f0395898f5084ad25b11a1fc7b8a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
